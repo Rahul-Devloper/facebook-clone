041762996d300d3df9beae788817abc15a01d880
Rahul-Devloper <rahulramesh26@yahoo.com>
2021-08-11 01:48:48 +0530

    created basic input submission using map

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [messages, setMessages] = useState(['hi', 'hello',])
+
+
+  console.log(input)
+  console.log(messages)
+
+
+  const sendMessage = (e)=>{
+    e.preventDefault();
+    setMessages([...messages, input])
+    setInput('')
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
+      <form action="">
+      <input type="text" value={input} onChange={(e)=> setInput(e.target.value)} />
+      <button type='submit' onClick={sendMessage}>Send Message</button>
+      </form>
+      
+
+    {
+      messages.map((message)=>(
+        <p>{message}</p>
+      ))
+    }
+
     </div>
   );
 }
