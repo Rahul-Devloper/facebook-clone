7354510198e54abbd5029fb4031fce9062afc27a
Rahul-Devloper <rahulramesh26@yahoo.com>
2021-08-12 00:12:48 +0530

    added css

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import { Card, CardContent, Typography } from "@material-ui/core";
+import React from "react";
+import './Message.css'
 
 const Message = (props) => {
-    return (
-        <div>
-            <h2>{props.username}: {props.text}</h2>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Card className='message'>
+        <CardContent>
+          <Typography className='color' gutterBottom>
+            {props.username}: {props.text}
+          </Typography>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
 
-export default Message
+export default Message;
