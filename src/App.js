e2d1be4a5c7e56b1c5a6d6cc05fb281658e7fcd8
Rahul-Devloper <rahulramesh26@yahoo.com>
2021-08-14 19:24:08 +0530

    pushing messages to collection

--- src/App.js
+++ src/App.js
@@ -4,32 +4,41 @@ import { useState } from "react";
 import "./App.css";
 import Message from "./components/Message";
 import db from "./Firebase";
+import firebase from "firebase";
 
 function App() {
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
   const [messages, setMessages] = useState([]);
-  const [username, setUsername] = useState('');
+  const [username, setUsername] = useState("");
 
   console.log(input);
   console.log(messages);
 
-  useEffect(()=>{
-    //this is the db 
-    db.collection('Fb-Messages').onSnapshot(snapshot=>{
-      setMessages(snapshot.docs.map(doc=> doc.data()))
-    })
-  }, [])
+  useEffect(() => {
+    //this is the db
+    db.collection("Fb-Messages")
+    .orderBy('timestamp', 'desc')
+    .onSnapshot((snapshot) => {
+      setMessages(snapshot.docs.map((doc) => doc.data()));
+    });
+  }, []);
 
-  useEffect(()=>{
-    setUsername(prompt('Please enter your name'))
-  }, [])
-
+  useEffect(() => {
+    setUsername(prompt("Please enter your name"));
+  }, []);
 
   const sendMessage = (e) => {
     e.preventDefault();
-    setMessages([...messages, {username: username, text: input}]);
+
+    db.collection("Fb-Messages").add({
+      text: input,
+      username: username,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+    // setMessages([...messages, {username: username, text: input}]);
     setInput("");
   };
+
   return (
     <div className="App">
       <h1>Hello World</h1>
